6d0ccaa33d0ab9e2c6cf1fc88dc1c3960b928ce2
JeonHaseung <tidnj1106@kaist.ac.kr>
2020-07-18 17:19:42 +0900

    Chapter 2

---
+++ README.md
@@ -0,0 +1,2 @@
+main : https://cs193p.sites.stanford.edu/
+youtube : https://www.youtube.com/playlist?list=PLpGHT1n4-mAtTj9oywMWoBx0dCGd51_yG
---
+++ chapter-1/Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by HASEUNG JEON on 2020/07/18.
+//  Copyright © 2020 HASEUNG JEON. All rights reserved.
+//
+
+import Foundation
+
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "😀", "😇", "😂", "😅", "😆", "🥰", "😘", "😍"]
+        return MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+            
+        }
+    }
+    
+    // MARK: - Access to the Model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
