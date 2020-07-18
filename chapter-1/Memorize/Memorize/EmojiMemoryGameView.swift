6b6881c31f3e9d443b9384d265d2bdfc6828cc92
JeonHaseung <tidnj1106@kaist.ac.kr>
2020-07-18 21:06:50 +0900

    chapter 3

---
+++ chapter-1/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,64 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by HASEUNG JEON on 2020/07/17.
+//  Copyright © 2020 HASEUNG JEON. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        
+    }
+}
+
+
+struct CardView: View {
+    var  card: MemoryGame<String>.Card
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRaduis).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRaduis).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRaduis).fill()
+            }
+            
+        }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRaduis: CGFloat = 10
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}
